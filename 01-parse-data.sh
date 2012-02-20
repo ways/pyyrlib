559e620a4032fb7674e34e76a9cadfe9c3bcc216
Lars Falk-Petersen <dev@falk-petersen.no>
2012-02-20 23:05:22 +0100

    Import of norwegian names done.

--- 01-parse-data.sh
+++ 01-parse-data.sh
@@ -3,10 +3,10 @@
 #This file is used to parse location data with yr.no-URLs.
 #We will end up with countries.txt, verda2.txt. Import in db with 02-import-data.py
 
-#world
+echo "parse world"
 cut -f1,11 --output-delimiter=, verda.txt |sort |uniq > countries.txt
 cut -f1,4,11,18 --output-delimiter=, verda.txt > verda2.txt
 
-#norway
-cut -f 2,14 --output-delimiter=, noreg.txt |awk 'BEGIN { FS = ":" } {print "NO,",$0 }' > verda2.txt
-
+echo "parse norway"
+tail -n+2 noreg.txt |cut -f 2,14 --output-delimiter=, |awk 'BEGIN { FS = "," } {print "NO,",$1,",Norway,",$2 }' >> verda2.txt
+echo "NO,Norway" >> countries.txt
