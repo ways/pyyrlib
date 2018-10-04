1fcddcb8e40f1e39464c8a677ab19aae41010358
Lars Falk-Petersen <lars.falk-petersen@copyleft.no>
2018-10-04 23:23:07 +0200

    Migrate to mysql connect

--- 02-import-data.py
+++ 02-import-data.py
@@ -5,12 +5,13 @@
 Put verda2.txt in mysql db
 '''
 
-import string, MySQLdb, sys
+import string, sys # MySQLdb
+import mysql.connector # sudo pip install mysql-connector
 
 def get_db_cursor ():
-  conn=MySQLdb.connect(host = "localhost",
+  conn=mysql.connector.connect(host = "10.223.68.17",
                            user = "pyyrlib",
-                           passwd = "ifoo3aeshahN",
+                           passwd = "pyyrlib",
                            db = "pyyrlib")
   return conn, conn.cursor ()
 
@@ -30,7 +31,7 @@ def insert_row_countries (cursor, table, fields):
 
   query += " ) ON DUPLICATE KEY UPDATE countryname = '" + fields[1] + "' ;"
 
-  print query
+  print (query)
   return cursor.execute(query)
 
 
@@ -45,29 +46,30 @@ def insert_row_verda (cursor, conn, table, fields):
     elif 2 == i:
       continue
     else:
-      query += "'" + conn.escape_string(all_lower(fields[i].replace(' ', ''))) + "'"
+      #query += "'" + conn.fields[i].replace(' ', '') + "'"
+      query += '"' + fields[i].replace(' ', '') + '"'
 
   query += " ) ;"
 
-  print query
+  print (query)
   return cursor.execute(query)
 
 
 def process_file_countries (cursor):
-  fd = open( "countries.txt" )
+  fd = open( "/home/larsfp/git/pyyrlib/countries.txt" )
   content = fd.readline()
   while (content != "" ):
-    fields = string.split(content, ',')
+    fields = str.split(content, ',')
     insert_row_countries(cursor, 'countries', fields)
     content = fd.readline()
 
 
 def process_file_verda (cursor, conn):
-  fd = open( "verda2.txt" )
-  content = fd.readline() #header
+  fd = open( "/home/larsfp/git/pyyrlib/verda2.txt" )
+  #content = fd.readline() #header
   content = fd.readline()
   while (content != "" ):
-    fields = string.split(content, ',')
+    fields = str.split(content, ',')
     insert_row_verda(cursor, conn, 'verda', fields)
     content = fd.readline()
 
